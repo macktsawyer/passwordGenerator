3dab7e288ae4dd06a25610412bcb50d2dd7edbb3
macktsawyer <macktsawyer@yahoo.com>
2023-04-28 23:16:02 -0400

    Expanded list and fixed formatting

--- generator.py
+++ generator.py
@@ -1,9 +1,12 @@
 import random
 
 alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-         "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+         "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+         "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$",
+         "%", "^", "&", "*"]
 
 length = input('How long of a password would you like? ')
+
 
 def password_generator(leng):
     password = ''
@@ -12,5 +15,7 @@ def password_generator(leng):
         password = password + alpha[letter]
     print(password)
 
+
 def gen():
     password_generator(length)
+
