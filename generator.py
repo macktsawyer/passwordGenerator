55852b45a180dd96ef27564f5d6becc7e28ba36c
macktsawyer <macktsawyer@yahoo.com>
2023-04-29 12:44:35 -0400

    Added logging functionality

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
          "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
@@ -8,13 +9,19 @@ alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "
 length = input('How long of a password would you like? ')
 
 
+def log_password(word):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("password_log.txt", "a") as f:
+        f.write(f"{timestamp}: {word}\n")
+
+
 def password_generator(leng):
     password = ''
     for x in range(int(leng)):
         letter = random.randint(0, len(alpha) - 1)
         password = password + alpha[letter]
     print(password)
-
+    log_password(password)
 
 def gen():
     password_generator(length)
